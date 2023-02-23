c59d458983d415bccddf95a66ea78d4ed2d53900
_DG_ <damirgaynanov@gmail.com>
2023-02-23 20:17:33 +0400

    Done

---
+++ Ex_013/Program.cs
@@ -0,0 +1,40 @@
+﻿// string[,] table = new string[2, 5];
+
+// table[1, 2] = "Text";
+// for (int rows= 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+
+int[,] matrix = new int[4, 4];
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1,10);// [1; 10)
+        }
+    }
+}
